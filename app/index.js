be0e2d779768d1ad806d076f61e7172381b8aa93
Raissa Oliveira <raissa.menezesousa@gmail.com>
2024-09-11 12:28:00 -0300

    ajuste na rota no .json

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ let metas
 
 const carregarMetas = async () => {
     try {
-        const dados = await fs.readFile("metas.json", "utf-8")
+        const dados = await fs.readFile("app/metas.json", "utf-8")
         metas = JSON.parse(dados)
     }
     catch (erro) {
@@ -16,7 +16,7 @@ const carregarMetas = async () => {
 }
 
 const salvarMetas = async() => {
-    await fs.writeFile("metas.json", JSON.stringify(metas, null,2))
+    await fs.writeFile("app/metas.json", JSON.stringify(metas, null,2))
 }
 
 const cadastrarMeta = async () => {
